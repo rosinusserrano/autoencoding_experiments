8349831e2ffaf0a31155614d5a71e529aecfb129
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2024-10-21 10:58:53 +0200

    init in distributions and new utils functoin

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,16 @@
 "Utility functions"
+import torch
 
-def make_animation():
-    # CODE FOR MAKING PLT ANIMATION
-    return False
+
+def generate_images(walk: torch.Tensor):
+    """
+    Generates images from all the points in the walk
+    """
+    pass
+
+
+def make_animation(images: torch.Tensor):
+    """
+    Takes as input the images and turns them into a matplotlib animation
+    """
+    pass
