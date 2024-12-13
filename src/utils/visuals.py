6bf16f0f401dc74414a3931b87f57c951f9d164b
Marco Rosinus Serrano <57681190+rosinusserrano@users.noreply.github.com>
2024-12-13 21:28:02 +0100

    wrong torch cat bitch

--- src/utils/visuals.py
+++ src/utils/visuals.py
@@ -24,7 +24,7 @@ def show_side_by_side(
     )
 
     return make_grid(
-        torch.cat(row1, row2, dim=2),
+        torch.cat((row1, row2), dim=2),
         nrow=1,
         padding=1,
         pad_value=1,
