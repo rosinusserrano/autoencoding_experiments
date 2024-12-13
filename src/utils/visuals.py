67dc405675be8710076a3858af28924d0322002f
Marco Rosinus Serrano <57681190+rosinusserrano@users.noreply.github.com>
2024-12-13 20:44:01 +0100

    function for plotting images side by side

---
+++ src/utils/visuals.py
@@ -0,0 +1,31 @@
+"""Functions for plotting."""
+
+import torch
+from torchvision.utils import make_grid
+
+
+def show_side_by_side(
+    tensor1: torch.Tensor,
+    tensor2: torch.Tensor,
+) -> torch.Tensor:
+    """Take to batches of images and show them side by side."""
+    row1 = make_grid(
+        tensor1,
+        normalize=True,
+        padding=1,
+        pad_value=1,
+    )
+
+    row2 = make_grid(
+        tensor2,
+        normalize=True,
+        padding=1,
+        pad_value=1,
+    )
+
+    return make_grid(
+        torch.cat(row1, row2, dim=2),
+        nrow=1,
+        padding=1,
+        pad_value=1,
+    )
