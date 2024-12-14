c4e18c6300220b2777a891ed5f5b8590865e6936
Marco Rosinus Serrano <57681190+rosinusserrano@users.noreply.github.com>
2024-12-14 17:06:04 +0100

    add save function to logger and create dedicated run directory

--- src/logger/base.py
+++ src/logger/base.py
@@ -4,6 +4,8 @@ from abc import ABC, abstractmethod
 from typing import Literal
 
 import torch
+from torch import nn
+from torch.optim import Optimizer
 
 
 class Logger(ABC):
@@ -27,20 +29,6 @@ class Logger(ABC):
         """Log a metric for a given epoch, which has a name and a value."""
         raise NotImplementedError
 
-    def log_metric(
-        self,
-        metric_name: str,
-        metric_value: float,
-        epoch: int,
-    ) -> None:
-        """Print metric to stdout and save to metrics dict for final plot."""
-        if metric_name not in self.metrics:
-            self.metrics[metric_name] = [metric_value]
-            self.metrics[f"{metric_name}_epochs"] = [epoch]
-        else:
-            self.metrics[metric_name].append(metric_value)
-            self.metrics[f"{metric_name}_epochs"].append(epoch)
-
     def log_grouped_metric(
         self,
         metric_name: str,
@@ -59,6 +47,15 @@ class Logger(ABC):
         method: Literal["append", "update"] = "append",
     ) -> None:
         """Log images."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def save(
+        self,
+        model: nn.Module | None = None,
+        optimizer: Optimizer | None = None,
+    ) -> None:
+        """Save model or optimizer, or both."""
         raise NotImplementedError
 
     @abstractmethod
--- src/logger/caveman.py
+++ src/logger/caveman.py
@@ -2,13 +2,15 @@
 
 import os
 from dataclasses import asdict, is_dataclass
-from os import PathLike
 from pathlib import Path
 from typing import Any, Literal
 
 import matplotlib.pyplot as plt
 import torch
 import yaml
+from human_id import generate_id
+from torch import nn
+from torch.optim import Optimizer
 from torchvision.utils import make_grid
 
 from logger.base import Logger
@@ -20,16 +22,23 @@ class CavemanLogger(Logger):
     def __init__(self, root_dir: str | Path) -> None:
         """Most basic logger: print to stdout and save to filesystem."""
         self.root_dir = Path(root_dir)
+        if not Path.exists(self.root_dir):
+            Path.mkdir(self.root_dir, parents=True)
 
-        self.plots_dir = Path(f"{root_dir}/plots")
+        run_index = len(os.listdir(self.root_dir))
+        human_id = generate_id(word_count=3)
+        run_id = f"{run_index:03d}-{human_id}"
+        self.run_dir = Path(f"{root_dir}/{run_id}")
+
+        self.plots_dir = Path(f"{self.run_dir}/plots")
         if not Path.exists(self.plots_dir):
             Path.mkdir(self.plots_dir, parents=True)
 
-        self.image_dir = Path(f"{root_dir}/images")
+        self.image_dir = Path(f"{self.run_dir}/images")
         if not Path.exists(self.image_dir):
             Path.mkdir(self.image_dir, parents=True)
 
-        self.config_dir = Path(f"{root_dir}/configs")
+        self.config_dir = Path(f"{self.run_dir}/configs")
         if not Path.exists(self.config_dir):
             Path.mkdir(self.config_dir, parents=True)
 
@@ -73,7 +82,7 @@ class CavemanLogger(Logger):
             self.metrics[metric_name].append(metric_value)
             self.metrics[f"{metric_name}_epochs"].append(epoch)
 
-        print(f"EPOCH {epoch}: {metric_name} {metric_value:.4f}")
+        print(f"EPOCH {epoch}: {metric_name} {metric_value:.4f}")  # noqa: T201
 
     def log_grouped_metric(
         self,
@@ -137,3 +146,15 @@ class CavemanLogger(Logger):
             plt.plot(epochs, values)
             plt.title(metric_name)
             plt.savefig({self.plots_dir})
+
+    def save(
+        self,
+        model: nn.Module | None = None,
+        optimizer: Optimizer | None = None,
+    ) -> None:
+        """Save model and optimizer,both optional."""
+        if model is not None:
+            torch.save(model.state_dict(), f"{self.run_dir}/model.pth")
+
+        if optimizer is not None:
+            torch.save(optimizer.state_dict(), f"{self.run_dir}/optimizer.pth")
